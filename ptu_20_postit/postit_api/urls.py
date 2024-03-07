2256be9b000b77996e5d73fdf5ef0b3bdd5595a2
virginijus10 <virgijijus.baranauskas@yahoo.com>
2024-03-07 14:40:06 +0200

    3

--- ptu_20_postit/postit_api/urls.py
+++ ptu_20_postit/postit_api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<int:pk>/comments/', views.CommentList.as_view()),
     path('comment)/<int:pk>/', views.CommentDetail.as_view()),
     path('<int:pk>/like/', views.PostLikeCreate.as_view()),
+    path('signup/', views.UserCreate.as_view()),
 ]
--- ptu_20_postit/ptu_20_postit/urls.py
+++ ptu_20_postit/ptu_20_postit/urls.py
@@ -18,12 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('', include('postit_api.urls'))
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('', include('postit_api.urls')),   
 ]
 
 urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
